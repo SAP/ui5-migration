136a08a5ea1418f62db575ee1f4a563c7892b1ee
Tobias Sorn <tobias.sorn@sap.com>
2019-10-31 16:54:24 +0100

    [FIX] Added failing tests

--- test/replaceGlobals/uriParams.expected.js
+++ test/replaceGlobals/uriParams.expected.js
@@ -20,15 +20,15 @@ sap.ui.define(["sap/base/util/UriParameters"],
 		 */
 		A.x = function(oParam, sContent) {
 			if (oParam.iTest === 47) {
-				oParam.doIt("background-image", "url('" + UriParameters.fromURL(window.location.href) + "')");
+				oParam.doIt("background-image", "url('" + UriParameters.fromQuery(window.location.search) + "')");
 			}
 
 
-			A.b(UriParameters.fromURL(window.location.href));
+			A.b(UriParameters.fromQuery(window.location.search));
 
 
-			A.m = String(UriParameters.fromURL(window.location.href));
-			A.m2 = String(UriParameters.fromURL(window.location.href).get(2));
+			A.m = String(UriParameters.fromQuery(window.location.search));
+			A.m2 = String(UriParameters.fromQuery(window.location.search).get(2));
 			A.m3 = String(UriParameters.fromURL(sContent || window.location.href).get(2));
 		};
 
