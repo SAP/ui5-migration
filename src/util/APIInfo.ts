cf4c2c1231b86056388bad28303412518a24f2b4
Merlin Beutlberger <m.beutlberger@sap.com>
2020-08-14 11:05:34 +0200

    [INTERNAL] APIInfo.ts: Remove OpenUI5 copyright header
    The OpenUI5 repository never contained this code

--- src/util/APIInfo.ts
+++ src/util/APIInfo.ts
@@ -1,9 +1,3 @@
-/* !
- * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2016 SAP SE or an SAP affiliate company.
- * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
- */
-
 /* tslint:disable:no-console */
 
 "use strict";
